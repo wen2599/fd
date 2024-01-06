d5264069e4a21cca4a40919d5f9772f2acf23049
wen2599 <87487280+wen2599@users.noreply.github.com>
2024-01-06 22:59:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '	xiu.xw.cloudns.biz'
+      url.hostname = '	wen9527.serv00.net'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
